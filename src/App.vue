4dac865192c9a666e9791e3bca1a4899c85d0989
ophx <castytastyhappybat@gmail.com>
2023-12-06 01:29:29 -0500

    first commit :)

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <div class="container mx-auto mt-4">
+    <!-- Navbar -->
+    <Navbar />
+
+    <!-- Main -->
+  </div>
+</template>
