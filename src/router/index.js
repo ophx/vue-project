a94a30b4ae1d3a33a5e382788dc8a1ac24560ac5
ophx <castytastyhappybat@gmail.com>
2023-12-06 18:46:02 -0500

    mmm

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import NotFound from "../views/errors/NotFound.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
+    { path: "/", component: Home },
+    { path: "/:pathMatch(.*)", component: NotFound },
 ];
 
 const router = createRouter({
-    history: createWebHistory("/"),
+    history: createWebHistory(),
     routes,
 });
 
---
+++ src/views/errors/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <div class="space-y-4 text-center w-80">
+            <img src="https://cdn3.emoji.gg/emojis/6426-error.png" class="h-24 w-24 shadow-lg mx-auto">
+            <p class="text-white text-xl font-medium">Well that's unexpected!</p>
+            <p class="text-gray-400">The page you were looking for was not found...</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
